ed528bc4cb823da356fe38c01747d016f2ea18bb
Gavin Elster <gavin.elster@me.com>
2016-02-15 19:06:14 -0800

    Disable source map for production

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -17,7 +17,7 @@ module.exports = {
   plugins: [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
+      sourceMap: false,
       compress: {
         warnings: false
       }
