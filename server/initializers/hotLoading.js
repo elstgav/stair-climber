4d04d3c5ed3a0546927f91c11a81ac1b015b11ad
Gavin Elster <gavin.elster@me.com>
2016-02-26 16:52:30 -0800

    Create initializers for db and hot loading

---
+++ server/initializers/hotLoading.js
@@ -0,0 +1,15 @@
+import webpack from 'webpack'
+import webpackConfig from '../../webpack.config.dev'
+import webpackDevMiddleware from 'webpack-dev-middleware'
+import webpackHotMiddleware from 'webpack-hot-middleware'
+
+export function init(app) {
+  if (process.env.NODE_ENV == 'production') return
+
+  const compiler = webpack(webpackConfig)
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath
+  }))
+  app.use(webpackHotMiddleware(compiler))
+}
