890319d34cafc27d16147216744ce7c6c24d5f95
Gavin Elster <gavin.elster@me.com>
2016-06-29 14:11:27 -0700

    Fix eslint errors

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -11,23 +11,23 @@ module.exports = _.extend({}, base, {
     // "only" prevents reload on syntax errors
     'webpack/hot/only-dev-server',
 
-    base.entry
+    base.entry,
   ],
 
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('development')
-      }
-    })
+        NODE_ENV: JSON.stringify('development'),
+      },
+    }),
   ],
 
   module: {
     loaders: base.module.loaders.concat({
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'babel?presets[]=react-hmre'
-    })
-  }
+      loader: 'babel?presets[]=react-hmre',
+    }),
+  },
 })
