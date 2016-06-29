63bdbc8461b71525df4691580d39709a7c1556fe
Richard Zhao <rzhao@avvo.com>
2016-06-29 12:45:56 -0700

    Reorganize leaderboard

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export DatePicker  from './DatePicker'
 export FlightsForm from './FlightsForm'
-export Leaderboard from './Leaderboard'
+export Leaderboard from './Leaderboard/Leaderboard'
 export Login from './Account/Login'
 export Register from './Account/Register'
--- src/pages/LeaderboardPage.js
+++ src/pages/LeaderboardPage.js
@@ -1,76 +1,15 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import api from '_/src/lib/api'
-
-/* import {
- *   Leaderboard
- * } from '_/src/components'*/
-
-const LeaderboardListElement = React.createClass({
-  render() {
-    return (
-      <tr>
-        <td>{this.props.leader.rank}</td>
-        <td>{this.props.leader.name}</td>
-        <td>{this.props.leader.elevation}</td>
-      </tr>
-    )
-  }
-})
-
-const LeaderboardList = React.createClass({
-  render() {
-    return (
-      <table>
-        <tbody>
-        {this.props.leaders.map((leader, index) =>
-          <LeaderboardListElement rank={index} leader={leader} />)
-        }
-        </tbody>
-      </table>
-    )
-  }
-})
-
-const Leaderboard = React.createClass({
-  render() {
-    return (
-      <div>
-        <h2>{this.props.title}</h2>
-        <LeaderboardList leaders={this.props.leaders} />
-      </div>
-    )
-  }
-})
-
+import { LeaderboardContainer } from '_/src/containers'
 
 export default React.createClass({
-
-  getInitialState() {
-    /* return {leaders: []}*/
-    return {
-      monthlyLeaders: [
-        {
-          name: "Pavel Sorokin",
-          elevation: 4800
-        }
-      ],
-      weeklyLeaders: []
-    }
-  },
-
-  componentWillMount() {
-    /* api
-     *   .get('/leaderboard/2016/01/')
-     *   .then(({ data }) => {this.setState({leaders: data})})*/
-  },
 
   render() {
     return (
       <div>
         <Helmet title='Leaderboard'/>
-        <Leaderboard title="March leaderboard" leaders={this.state.monthlyLeaders}/>
-        <Leaderboard title="Weekly leaders" leaders={this.state.weeklyLeaders}/>
+        <LeaderboardContainer />
       </div>
     )
   }
