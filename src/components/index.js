b8f9254d8697705ef167c24021bc6af572d47a63
Richard Zhao <rzhao@avvo.com>
2016-06-29 12:47:56 -0700

    Fix merge conflict

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 export DatePicker  from './DatePicker'
 export FlightsForm from './FlightsForm'
 export Leaderboard from './Leaderboard/Leaderboard'
-export Login from './Account/Login'
 export LoginForm from './Account/LoginForm'
 export Register from './Account/Register'
 export OldLeaderboard from './OldLeaderboard'
