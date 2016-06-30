378ea914e92c5803f54ff00cc661580586c19ad1
Richard Zhao <rzhao@avvo.com>
2016-06-30 13:22:06 -0700

    Use require to handle stylesheet

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import routes from './routes'
 import { render } from 'react-dom'
 import { Router, browserHistory } from 'react-router'
 
+require('./stylesheets/main.scss')
+
 const dest = document.getElementById('app')
 
 render((
