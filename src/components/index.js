18ce10b79962a34a2bcabf7dcc186e80c8cc31aa
Gavin Elster <gavin.elster@me.com>
2016-06-30 13:50:10 -0700

    Move components into namespace folder

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,10 @@
-export { DatePicker }  from './DatePicker'
-export { FlightsForm } from './FlightsForm'
-export { Leaderboard } from './Leaderboard/Leaderboard'
-export { LoginForm } from './Account/LoginForm'
-export { RegisterForm } from './Account/RegisterForm'
-export { OldLeaderboard } from './OldLeaderboard'
-export { ErrorMessageBanner } from './ErrorMessageBanner'
+export { LoginForm }                  from './Account/LoginForm'
+export { RegisterForm }               from './Account/RegisterForm'
+
+export { DatePicker }                 from './FlightTracker/DatePicker'
+export { FlightsForm }                from './FlightTracker/FlightsForm'
+export { OldLeaderboard }             from './FlightTracker/OldLeaderboard'
+
+export { Leaderboard }                from './Leaderboard/Leaderboard'
+
+export { ErrorMessageBanner }         from './ErrorMessageBanner'
