4f765290d9ac33dfff0a09d8496448f683881ae2
Richard Zhao <rzhao@avvo.com>
2016-07-01 10:03:07 -0700

    Use firebase to get/set flights

--- src/pages/FlightTrackerPage.js
+++ src/pages/FlightTrackerPage.js
@@ -10,8 +10,8 @@ import { getFirebase } from 'src/lib/firebaseAdapter'
 import {
   DatePicker,
   FlightsForm,
-  OldLeaderboard,
-  UserPicker,
+  /* OldLeaderboard,
+   * UserPicker,*/
 } from 'src/components'
 
 
@@ -30,7 +30,8 @@ export class FlightTrackerPage extends React.Component {
         getFirebase().database()
           .ref(`users/${user.uid}`)
           .on('value', snapshot => {
-            this.setState({ user: snapshot.val() })
+            this.setState({ user: Object.assign({}, snapshot.val(), { uid: user.uid }) })
+            this.getFlightsClimbed(this.state.user.uid, this.state.entryDate.format('YYYY-MM-DD'))
           })
       }
     })
@@ -45,10 +46,32 @@ export class FlightTrackerPage extends React.Component {
     this.setState({
       person: this.state.person,
     })
+    this.setFlightsClimbed(flights, this.state.user.uid, this.state.entryDate.format('YYYY-MM-DD'))
   }
 
   onPersonChanged = (person) => {
     this.setState({ person })
+  }
+
+  getFlightsClimbed = (uid, date) => {
+    getFirebase().database()
+                 .ref(`flights/${uid}/${date}`)
+                 .once('value')
+                 .then(snapshot => {
+                   const data = snapshot.val()
+                   console.log('Getting:', `flights/${uid}/${date}`, data)
+                   this.state.person.flightsClimbed.set(this.state.entryDate, data.flights_climbed)
+                   this.setState({
+                     person: this.state.person,
+                   })
+                 })
+  }
+
+  setFlightsClimbed = (flights, uid, date) => {
+    console.log('Storing:', `flights/${uid}/${date}`, flights)
+    getFirebase().database()
+           .ref(`flights/${uid}/${date}`)
+           .update({ flights_climbed: flights })
   }
 
   render() {
@@ -59,7 +82,6 @@ export class FlightTrackerPage extends React.Component {
         <h1>StepUp</h1>
 
         {this.state.user && <p>Hello {this.state.user.name}!</p>}
-        <UserPicker user={this.state.person} onChange={this.onPersonChanged} />
 
         <DatePicker
           selected={this.state.entryDate}
@@ -70,8 +92,6 @@ export class FlightTrackerPage extends React.Component {
           value={this.state.person.flightsClimbed.get(this.state.entryDate)}
           onChange={this.onFlightsChanged}
         />
-
-        <OldLeaderboard people={People} flightTracker={FlightTracker} />
       </div>
     )
   }
